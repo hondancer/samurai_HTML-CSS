92da47517a2813cf829d4db56eb70596eddb287c
EC2 Default User <ec2-user@ip-172-31-18-48.ap-northeast-1.compute.internal>
2021-10-26 14:08:55 +0000

    first commit

---
+++ restaurants_searcher.rb
@@ -0,0 +1,10 @@
+def write_date_to_csv()
+    restaurants = []
+    response = {hogehoge:"hogehoge"}
+    if response==nil or response["results"].has_key?("error") then
+        puts "エラーが発生しました！"
+    end
+    return prit restaurants
+end
+
+write_data_to_csv()
